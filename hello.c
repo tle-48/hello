2248deef88831ecb8c7364f49407aa096d495ba6
Tran Hai Khanh Le <tle48@huskers.unl.edu>
2022-08-25 19:55:12 +0000

    added my major to the out put

---
+++ hello.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Tran Hai Khanh Le
+ *         tle48@huskers.unl.edu
+ *
+ *
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tran Hai Khanh Le!!!!!\n");
+  printf("electrical engineering.....\n");
+
+  return 0;
+}
